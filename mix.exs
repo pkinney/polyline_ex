9a524d162b105e121a4f7438c02824e03414573f
Powell Kinney <628952+pkinney@users.noreply.github.com>
2020-06-27 09:16:10 -0500

    Bump version to 1.2.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Polyline.Mixfile do
   def project do
     [
       app: :polyline,
-      version: "1.2.1",
+      version: "1.2.2",
       elixir: "~> 1.3",
       description: description(),
       package: package(),
