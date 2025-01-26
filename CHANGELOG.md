74ab76e7c899c6b57ee3978af0e040fa7e1c77b6
Powell Kinney <628952+pkinney@users.noreply.github.com>
2025-01-26 13:58:27 -0600

    Bump version to 1.5.0

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Change Log
+
+## [1.5.0] - 2025-01-26
+
+### Changed
+
+- Pefromance improvements for `decode/2` (https://github.com/pkinney/polyline_ex/pull/15)
--- doc/dist/sidebar_items-8fb685daf6.js
+++
@@ -1,1 +0,0 @@
-sidebarNodes={"extras":[{"id":"api-reference","title":"API Reference","group":"","headers":[{"id":"Modules","anchor":"modules"}]}],"exceptions":[],"modules":[{"id":"Polyline","title":"Polyline","group":"","functions":[{"id":"decode/2","anchor":"decode/2"},{"id":"encode/2","anchor":"encode/2"}]}],"tasks":[]}
